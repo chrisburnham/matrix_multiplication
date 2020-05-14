30524da3d97933035060294fefe857ec4b3b7290
Chris Burnham <cburnham@greensea.com>
2020-05-14 07:34:39 -0400

    adding mpi test functions

--- main.cpp
+++ main.cpp
@@ -16,6 +16,26 @@
 // Namespace shortcut
 namespace chr = std::chrono;
 
+void timestamp()
+{
+# define TIME_SIZE 40
+
+  static char time_buffer[TIME_SIZE];
+  const struct std::tm *tm_ptr;
+  std::time_t now;
+
+  now = std::time ( NULL );
+  tm_ptr = std::localtime ( &now );
+
+  std::strftime ( time_buffer, TIME_SIZE, "%d %B %Y %I:%M:%S %p", tm_ptr );
+
+  std::cout << time_buffer << "\n";
+
+  return;
+# undef TIME_SIZE
+}
+
+
 int main( int argc, char *argv[] )
 {
     FILE* file_a;
@@ -26,6 +46,74 @@ int main( int argc, char *argv[] )
     {
         printf( "Error: Expected the names of two matricies to multiply.\n" );
         return EXIT_FAILURE;
+    }
+
+    int id;
+    int ierr;
+    int p;
+    double wtime;
+  //
+  //  Initialize MPI.
+  //
+    ierr = MPI_Init ( &argc, &argv );
+
+    if ( ierr != 0 )
+    {
+      cout << "\n";
+      cout << "HELLO_MPI - Fatal error!\n";
+      cout << "  MPI_Init returned nonzero ierr.\n";
+      exit ( 1 );
+    }
+  //
+  //  Get the number of processes.
+  //
+    ierr = MPI_Comm_size ( MPI_COMM_WORLD, &p );
+  //
+  //  Get the individual process ID.
+  //
+    ierr = MPI_Comm_rank ( MPI_COMM_WORLD, &id );
+
+    if ( id == 0 )
+    {
+      timestamp ( );
+      cout << "\n";
+      cout << "P" << id << ":  HELLO_MPI - Master process:\n";
+      cout << "P" << id << ":    C++/MPI version\n";
+      cout << "P" << id << ":    An MPI example program.\n";
+      cout << "\n";
+      cout << "P" << id << ":    The number of processes is " << p << "\n";
+      cout << "\n";
+    }
+  //
+  //  Every process prints a hello.
+  //
+    if ( id == 0 )
+    {
+      wtime = MPI_Wtime ( );
+    }
+    cout << "P" << id << ":    'Hello, world!'\n";
+  //
+  //  Process 0 says goodbye.
+  //
+    if ( id == 0 )
+    {
+      wtime = MPI_Wtime ( ) - wtime;
+      cout << "P" << id << ":    Elapsed wall clock time = " << wtime << " seconds.\n";
+    }
+  //
+  //  Terminate MPI.
+  //
+    MPI_Finalize ( );
+  //
+  //  Terminate.
+  //
+    if ( id == 0 )
+    {
+      cout << "\n";
+      cout << "P" << id << ":  HELLO_MPI:\n";
+      cout << "P" << id << ":    Normal end of execution.\n";
+      cout << "\n";
+      timestamp ( );
     }
 
     if( (file_a = fopen( argv[1], "r" )) == NULL )
